8f65360a6e5cfdd815965290896e8f37a4955aaf
Abdel-Nasr <abdelnassam@gmail.com>
2025-11-07 16:30:12 -0500

    Main gameplay loop established
    All animations added along with functional block, attack, and movement mechanics

--- shoot/Global.gd
+++ shoot/Global.gd
@@ -1,18 +1,31 @@
 extends Node
 
+var player_can_move = false
+@onready var announcer = $"/root/Main/UI/Announcer"
+@onready var match_time = $"/root/Main/UI/MatchBeginTime"
+
 func _ready() -> void:
 	#Global.match_countdown()
 	pass
 
 func match_countdown():
-	$"/root/Main/UI/MatchBeginTime".show()
+	match_time.show()
 	var i: int = 3
 	while(i > 0):
-		$"/root/Main/UI/MatchBeginTime".set_text(str(i))
+		match_time.set_text(str(i))
 		await get_tree().create_timer(1).timeout 
 		i-=1
-	$"/root/Main/UI/MatchBeginTime".set_text("SHOOT!")
+	match_time.set_text("SHOOT!")
 	await get_tree().create_timer(1).timeout 
-	$"/root/Main/UI/MatchBeginTime".hide()
+	player_can_move = true
+	match_time.hide()
 	
+func takedown(player):
+	announcer.show()
+	announcer.set_text("TAKEDOWN %s! " % player)
+	await get_tree().create_timer(0.3).timeout 
+
+func match_over(winner, loser):
+	announcer.show()
+	announcer.set_text("MATCH OVER! \n %s HAS WON!")
 	
--- shoot/Scenes/wrestler1.gd
+++ shoot/Scenes/wrestler1.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED: float = 5.0
-const SHOTSPEED: float = 100.0
+const SPEED: float = 300.0
+const SHOTSPEED: float = 5000.0
 const STUFFEDSPEED: float = 2.0
 var is_blocking: bool = false
 var is_shooting: bool = false
@@ -10,7 +10,8 @@ var can_shoot: bool = true
 var can_move: bool = false
 var shotbar = 3
 var block_stun_time: float = 0.6
-var stuffed_stun_time: float = 0.3
+var stuffed_stun_time: float = 1
+var shoot_prep_time: float = 0.6
 
 
 func _ready() -> void:
@@ -30,66 +31,72 @@ func _physics_process(delta: float) -> void:
 #		can_shoot = false
 		
 	
-	var direction : float = Input.get_axis("p1_left", "p1_right")
-	
+
+	var direction = 0
 	var last_direction = 0
 	
 	#Inputs and movement
 	
 	if is_shooting:
 		can_move = false
+	elif is_blocking:
+		can_move = false
 	else:
 		can_move = true
 	
-	if velocity.x == 0:
+	if velocity.x == 0 and can_move:
 		$Anims.play("Idle_anim")
 		
 	if can_move:
-		if Input.is_action_pressed("p1_block"):
-			is_blocking  = true
-			$Anims.play("Stuff_anim")
+		if Input.is_action_pressed("p1_left"):
+			direction = -1 
+			$Anims.play("Walk_anim")
+		if Input.is_action_pressed("p1_right"):
+			direction = 1 
+			$Anims.play("WalkBack_anim")
+		
+		if Input.is_action_just_pressed("p1_block"):
 			block()
-			is_blocking  = false
 			
-		if Input.is_action_just_pressed("p1_shoot") and direction < 0 and can_shoot:
+		if Input.is_action_just_pressed("p1_shoot") and velocity.x < 0 and can_shoot:
 			is_shooting = true
 			$Anims.play("Shoot_anim")
-			shoot(delta)
+			await get_tree().create_timer(shoot_prep_time).timeout
+			velocity.x = -3 * SHOTSPEED
+			move_and_slide()
+			shoot()
 			await get_tree().create_timer(1).timeout 
 			is_shooting = false
-			
-		
-		if direction:
-			velocity.x = direction * SPEED
-			$Anims.play("Walk_anim")
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-		move_and_collide(velocity)
-
-func shoot(delta: float) -> void:
 	
-	$Anims.play("Shoot_anim")
+		velocity.x = direction * SPEED
 
+		move_and_slide()
 
-	velocity.x -= SHOTSPEED
-	var collider = move_and_collide(velocity)
-	if collider && collider.get_collider().is_class("CharacterBody2D"):
+func shoot() -> void:
+
+	for i in get_slide_collision_count():
 		
-		if collider.get_collider().is_blocking:
-			$Anims.play("Stuff_anim")
-			await get_tree().create_timer(stuffed_stun_time).timeout
-			print("STUFFED")
-			velocity.x = move_toward(-velocity.x, -SHOTSPEED, 2)
-			move_and_collide(velocity)
+		var collider = get_slide_collision(i)
+		
+		if collider && collider.get_collider().is_class("CharacterBody2D"):
 			
-			
-	shotbar -= 1
-	
+			if collider.get_collider().is_blocking:
+				$Anims.play("Whiff_anim")
+				
+				await get_tree().create_timer(stuffed_stun_time).timeout
+				print("STUFFED")
+				velocity.x = move_toward(-velocity.x, -SHOTSPEED, 4)
+				
+				
+				
+		shotbar -= 1
+		
 	
 func block() -> void:
-	can_move = false
+	is_blocking  = true
+	$Anims.play("Stuff_anim")
 	await get_tree().create_timer(block_stun_time).timeout 
+	is_blocking  = false
 	can_move = true
 
 
