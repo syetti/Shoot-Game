6729e0d9cbfe88ea8a9a3363e39fe8793ea1eee6
Abdel <abdelnassam@gmail.com>
2025-11-13 17:59:07 -0500

    Transferred movement to network dependence

--- shoot/Global.gd
+++ shoot/Global.gd
@@ -3,24 +3,46 @@ extends Node
 var player_can_move = false
 @onready var announcer = $"/root/Main/UI/Announcer"
 @onready var match_time = $"/root/Main/UI/MatchBeginTime"
+@onready var client_button = $"/root/Main/UI/client"
+@onready var server_button = $"/root/Main/UI/server"
+@onready var status = $"/root/Main/UI/network_status"
+
+
+signal server_disconnected
+
 
 func _ready() -> void:
+	
+	client_button.pressed.connect(_on_client_button_pressed)
+	server_button.pressed.connect(_on_server_button_pressed)
+	
+	multiplayer.peer_connected.connect(_on_player_connected)
+	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	multiplayer.connected_to_server.connect(_on_connected_ok)
+	multiplayer.connection_failed.connect(_on_connected_fail)
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
+
+
 	#Global.match_countdown()
 	pass
-	
-func matchStart() -> void:
-	#Create Server
-	@warning_ignore("standalone_expression")
-	multiplayer
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(9999, 1)
-	multiplayer.multiplayer_peer = peer
-	
+
+func _on_client_button_pressed():
 	#Create Client
-	peer = ENetMultiplayerPeer.new()
-	peer.create_client("129.32.224.72", 9999)
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client("127.0.0.1", 9999 )
 	
 	multiplayer.multiplayer_peer = peer
+	status.text = "Connecting"
+	pass
+func _on_server_button_pressed():
+	
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(9999 , 1)
+	multiplayer.multiplayer_peer = peer
+	status.text = "Waiting"
+
+	
+	
 func match_countdown():
 	match_time.show()
 	var i: int = 3
@@ -42,4 +64,20 @@ func match_over(winner):
 	announcer.show()
 	announcer.set_text("MATCH OVER! \n %s HAS WON!", winner)
 	
+func _on_player_connected(id: int):
+	status.text = "Connected"
 	
+func _on_player_disconnected():
+	status.text = "Disconnected"
+func _on_connected_ok():
+	var peer_id = multiplayer.get_unique_id()
+	pass
+
+
+func _on_connected_fail():
+	pass
+
+
+func _on_server_disconnected():
+	
+	server_disconnected.emit()
--- shoot/Scenes/wrestler1.gd
+++ shoot/Scenes/wrestler1.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-
+var input_prefix := "p1_"
 const SPEED: float = 300.0
 const SHOTSPEED: float = 5000.0
 const STUFFEDSPEED: float = 2.0
+var shoot_prep_ticks := 18  # ~0.3 seconds at 60 ticks/sec
+var shoot_cooldown_ticks := 60  # ~1 second at 60 ticks/sec
+var shoot_timer := 0
+var shoot_cooldown := 0
 var is_blocking: bool = false
 var is_shooting: bool = false
 var can_shoot: bool = true
@@ -13,6 +17,8 @@ var block_stun_time: float = 0.6
 var stuffed_stun_time: float = 1
 var shoot_prep_time: float = 0.6
 
+@onready var anims = $Anim
+
 
 func _ready() -> void:
 	
@@ -21,15 +27,78 @@ func _ready() -> void:
 	
 	
 	
-func _physics_process(delta: float) -> void:
+func _get_local_input() -> Dictionary:
 	
 	
+	var input := {}
+	if can_move:
+		if Input.is_action_pressed(input_prefix + "left"):
+			input["left"] = true
+		if Input.is_action_pressed(input_prefix + "right"):
+			input["right"] = true
+		if Input.is_action_just_pressed(input_prefix + "block"):
+			input["block"] = true
+		if Input.is_action_just_pressed(input_prefix + "shoot"):
+			input["shoot"] = true
+	
+	return input
+func _network_process(input: Dictionary) -> void:
 	
 	#EVERYTIME PLAYER SHOOTS, BAR DROPS 1
 	#IF PLAYER SHOOTS INTO BLOCK, BAR DROPS 0.5, INCENTIVIZE AGGRESSION, DISCOURAGE SPAMMING
 	#if shotbar == 0:
 #		can_shoot = false
 		
+	# Update timers
+	if shoot_timer > 0:
+		shoot_timer -= 1
+		
+			
+	if shoot_cooldown > 0:
+		shoot_cooldown -= 1
+	
+	# Reset direction
+	var direction = 0
+	
+	if can_move:
+		# Handle movement inputs
+		if input.get("left", false):
+			direction = -1
+			if not is_shooting:
+				anims.play("Walk_anim")
+		elif input.get("right", false):
+			direction = 1
+			if not is_shooting:
+				anims.play("WalkBack_anim")
+		
+		# Handle block input
+		if input.get("block", false):
+			block()
+		
+		# Handle shoot input
+		if input.get("shoot", false) and not is_shooting :
+			if shoot_cooldown == 0:
+				is_shooting = true
+				shoot()
+				shoot_cooldown = shoot_cooldown_ticks
+				anims.play("Shoot_anim")
+	
+	# Apply movement
+	if is_shooting:
+		velocity.x = -3 * SHOTSPEED
+	else:
+		velocity.x = direction * SPEED
+	
+	# Idle animation when not moving
+	if velocity.x == 0 and can_move and not is_shooting:
+		anims.play("Idle_anim")
+	
+	move_and_slide()
+func _physics_process(delta: float) -> void:
+	
+	
+	
+	
 	
 
 	var direction = 0
