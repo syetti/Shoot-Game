c77fe6cea95ae09bb596771a4f01a7b321e48d61
Abdel-Nasr <abdelnassam@gmail.com>
2026-01-26 17:53:46 -0500

    Entire System Reprogram
    State machines everywhere

--- shoot/Scenes/Dummy.gd
+++ shoot/Scenes/Dummy.gd
@@ -11,12 +11,45 @@ var is_shooting: bool = false
 var can_shoot: bool = true
 
 var shotbar = 3
+var current_state = State.BLOCK
+enum State{
+	WALK,
+	BLOCK,
+	SHOOT,
+	STUN
+}
 
+enum hit_state {
+	HIT,
+	BLOCKED,
+	INVUNERABLE
+}
 
 func _ready() -> void:
-	is_blocking = true
-	
-	
-	
-func _physics_process(delta: float) -> void:
+	current_state = State.BLOCK
+	match State:
+		State.WALK:
+			_handle_dummy_walk()
+		State.BLOCK:
+			_handle_dummy_block()
+		State.SHOOT:
+			_handle_dummy_shoot()
+		State.STUN:
+			_handle_dummy_stun()
+	current_state = State.BLOCK
+
+func _handle_dummy_walk():
 	pass
+func _handle_dummy_shoot():
+	pass
+func _handle_dummy_block():
+	pass
+func _handle_dummy_stun():
+	pass
+	
+func try_hit(dmg : int) -> hit_state:
+	if current_state == State.BLOCK:
+		return hit_state.BLOCKED
+	print("BEEN HIT")
+	velocity.x = 0
+	return hit_state.HIT
--- shoot/Scenes/wrestler1.gd
+++ shoot/Scenes/wrestler1.gd
@@ -2,32 +2,64 @@ extends CharacterBody2D
 
 # 1 = Facing Right (Player 1)
 # -1 = Facing Left (Player 2)
-@export var fixed_facing_dir: int = -1
+var fixed_facing_dir: int = -1
 
+#Speed
 const SPEED: float = 300.0
 const SHOTSPEED: float = 5000.0
 const STUFFEDSPEED: float = 2.0
-var shoot_prep_ticks := 18  # ~0.3 seconds at 60 ticks/sec
-var shoot_cooldown_ticks := 60  # ~1 second at 60 ticks/sec
-var shoot_timer := 0
+var shoot_distance: float = 400.0
+
+##Timers
+var state_timer = 0
+
+#Shooting
+var shoot_prep_time: int = 7
+var shoot_cooldown_time := 40  # ~2/3 second at 40 ticks/sec
+var shoot_recovery_time = 26 #On Miss
+
+var shoot_active_h_time := 5
+var shoot_active_m_time := 5
 var shoot_cooldown := 0
-var is_blocking: bool = false
-var is_shooting: bool = false
-var can_shoot: bool = true
+
 var shotbar = 3
-var block_stun_time: float = 0.6
-var stuffed_stun_time: float = 1
-var shoot_prep_time: float = 0.6
-var dir_multi = 0
+
+#Think in frames
+
+#Blocking
+var block_prep_time: int = 3
+var block_root_time: int = 15
+var block_cooldown_time: int = 20
+var block_cooldown = 0
+
+var stuffed_stun_time: float = 10
+var has_connected: bool = false
+ #5 frames
+
+###
+
+### Minor States
+var shoot_state = 0
+var block_state = 0
+var throw_state = 0
 
 
+
+###Major States
+
+enum hit_state {
+	HIT,
+	BLOCKED,
+	INVUNERABLE
+}
 
 enum State {
 	IDLE,
 	SHOOT,
 	BLOCK,
 	THROW,
-	WALK
+	WALK,
+	STUN
 }
 
 var current_state = State.IDLE
@@ -35,48 +67,59 @@ var current_state = State.IDLE
 
 
 func _ready() -> void:
+	
+	current_state = State.IDLE
+	
+
+func _network_spawn(data: Dictionary) -> void:
+	position = data.get("position", Vector2(180,400))
+	fixed_facing_dir = data.get("facing_dir", 1)
+	
 	if fixed_facing_dir == 1:
-		$Anims.flip_h = true
+		$Sprite.flip_h = true
+	else:
+		$Sprite.flip_h = false
+		
+	var owner_id = data.get("peer_id", 1)
+	set_multiplayer_authority(owner_id)
 	current_state = State.IDLE
 	
 #Finally learning state machines...
 func _network_process(input: Dictionary) -> void:
+	if state_timer > 0:
+		state_timer -=1
+		
 	
 	
 	#EVERYTIME PLAYER SHOOTS, BAR DROPS 1
 	#IF PLAYER SHOOTS INTO BLOCK, BAR DROPS 0.5, INCENTIVIZE AGGRESSION, DISCOURAGE SPAMMING
 	#if shotbar == 0:
 #		can_shoot = false
-
-
-
-	
+### Major States
 	match current_state:
 		State.IDLE:
 			_handle_idle_state(input)
 		State.SHOOT:
-			_handle_shoot_state(input)
+			_handle_shoot_state()
 		State.BLOCK:
-			_handle_block_state(input)
+			_handle_block_state()
 		State.THROW:
 			_handle_throw_state(input)
 		State.WALK:
 			_handle_walk_state(input)
-	# Update timers
-	if shoot_timer > 0:
-		shoot_timer -= 1
+		State.STUN:
+			_handle_stun_state()
 			
+			
+	# Update timers
 	if shoot_cooldown > 0:
 		shoot_cooldown -= 1
 	
-	###  Transition to IDLE
-	if velocity.x == 0:
-		current_state = State.IDLE
+	if block_cooldown > 0:
+		block_cooldown -=1
 		
 	###  MOVE
-	set_velocity(Vector2(SPEED, 0))
 	move_and_slide()
-	velocity = get_velocity()
 	
 
 func _get_local_input() -> Dictionary:
@@ -84,11 +127,17 @@ func _get_local_input() -> Dictionary:
 	
 	var input := {}
 	
+	# SECURITY CHECK:
+	# Only read inputs if *I* own this character.
+	# If I am Player 1, this returns False for the Player 2 character.
+	if not is_multiplayer_authority():
+		return {}
+	
 	input["block"] = Input.is_action_just_pressed("block")
 	input["shoot"] =  Input.is_action_just_pressed("shoot")
 	input["throw"] =  Input.is_action_just_pressed("throw")
 	input["move_x"] = Input.get_axis("left","right")
-			
+	
 	
 	return input
 	
@@ -97,7 +146,11 @@ func _save_state() -> Dictionary:
 	return{
 		position = position,
 		velocity =  velocity,
-		current_state = current_state
+		current_state = current_state,
+		state_timer = state_timer,
+		shoot_state = shoot_state,
+		shoot_cooldown = shoot_cooldown,
+		block_cooldown = block_cooldown
 	}
 	
 
@@ -105,64 +158,126 @@ func _load_state(state: Dictionary):
 	position = state['position']
 	velocity = state['velocity']
 	current_state = state['current_state']
+	state_timer = state['state_timer']
+	shoot_state = state['shoot_state']
+	
+	shoot_cooldown = state['shoot_cooldown']
+	block_cooldown = state['block_cooldown']
 	
 func _handle_idle_state(input: Dictionary) -> void:
-	anims.play("Idle_anim")
+	anims.play("idle")
 	
+	#Movement Transition
 	var move_dir = input.get("move_x", 0)
 	if move_dir !=0:
 		current_state = State.WALK
-	
+		velocity.x = move_dir * SPEED
+		return
+		
 	if input.get("block", false):
-		current_state = State.BLOCK
+		if block_cooldown == 0:
+			velocity.x = 0
+			$Anims.play("block_p_anim")
+			current_state = State.BLOCK
+			state_timer = block_root_time
 		
 	# Handle shoot input
-		if input.get("shoot", false):
-			if shoot_cooldown == 0:
-				velocity.x = 0
-				current_state = State.BLOCK
-				shoot_cooldown = shoot_cooldown_ticks
-				anims.play("Shoot_anim")
+	if input.get("shoot", false):
+			velocity.x = 0 
+			shotbar -= 1
+			shoot_state = 0
+			current_state = State.SHOOT
+			state_timer = shoot_prep_time
 
-
-func _handle_walk_state(input: Dictionary) -> void:
-	current_state = State.WALK
+func _handle_walk_state(input: Dictionary) -> void:	
 	# Handle movement inputs
 	var move_dir = input.get("move_x", 0)
+	
 	if move_dir !=0:
 		velocity.x = move_dir * SPEED
 		
 		if move_dir == fixed_facing_dir:
-			$Anims.play("Walk_anim")
+			anims.play("walk_f")
 		else:
-			$Anims.play("WalkBack_anim")
+			anims.play("walk_b")
+		
+		if input.get("shoot", false):
+			if shoot_cooldown <= 0:
+				shoot_state = 0
+				velocity.x = 0 
+				anims.play("shoot_anim/shoot_p")
+				state_timer = shoot_prep_time
+				current_state = State.SHOOT
+				
+			
+			
 	else:
 		velocity.x = 0
-	
+		current_state = State.IDLE
 	
 func _handle_throw_state(input: Dictionary) -> void:
 	pass
-func _handle_block_state(input: Dictionary) -> void:
-	is_blocking  = true
-	$Anims.play("Stuff_anim")
-	await get_tree().create_timer(block_stun_time).timeout 
-	is_blocking  = false
+func _handle_block_state() -> void:
+	$Anims.play("block_anim/block_a")
+	
+	if state_timer <= 0:
+		print("doneblock")
+		block_cooldown = block_cooldown_time
+		current_state = State.IDLE
 
-func _handle_shoot_state(input: Dictionary) -> void:
-	velocity.x = dir_multi* -3 * SHOTSPEED
-	for i in get_slide_collision_count():
+func _handle_shoot_state() -> void:
+	if state_timer > 0:
+		if shoot_state == 1:
+			var req_speed = shoot_distance/(shoot_active_m_time/60.0)
+			velocity.x = fixed_facing_dir * req_speed
+			###Collision
+			if not has_connected:
+				for i in get_slide_collision_count():
+					var collider = get_slide_collision(i)
+					var object = collider.get_collider()
+					if object and object.has_method("try_hit"):
+						var res = object.try_hit(1)
+						match res:
+							hit_state.HIT:
+								print("Hit_Target")
+								has_connected = true
+								velocity.x = 0
+								anims.play("hit")
+							hit_state.BLOCKED:
+								print("Stuffed")
+								has_connected = true
+								return
+						
+					return
+			return
+
+		match shoot_state:
+			0:
+				anims.play("shoot_anim/shoot_a")
+				state_timer = shoot_active_m_time
+				shoot_state = 1
+			1:
+				anims.play("shoot_anim/shoot_r")
+				state_timer = shoot_recovery_time
+				shoot_state = 2
+				velocity.x = 0
+			2:
+				velocity.x = 0
+				shoot_cooldown = shoot_cooldown_time
+				current_state = State.IDLE
+				
 		
-		var collider = get_slide_collision(i)
 		
-		if collider && collider.get_collider().is_class("CharacterBody2D"):
-			
-			if collider.get_collider().is_blocking:
-				$Anims.play("Whiff_anim")
-				
-				await get_tree().create_timer(stuffed_stun_time).timeout
-				print("STUFFED")
-				velocity.x = move_toward(-velocity.x, -SHOTSPEED, 4)
-				
-				
-				
 		shotbar -= 1
+	return
+func _handle_stun_state() -> void:
+	current_state = State.IDLE
+	pass
+
+func try_hit(dmg : int) -> hit_state:
+	if current_state == State.BLOCK:
+		return hit_state.BLOCKED
+	print("BEEN HIT")
+	velocity.x = 0
+	return hit_state.HIT
+	
